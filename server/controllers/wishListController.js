82b5e44b085a6e4bed9169e094ae917758c450dc
krishna-65 <sharmakrishnakant721@gmail.com>
2025-08-29 14:42:10 +0530

    make chnages

--- server/controllers/wishListController.js
+++ server/controllers/wishListController.js
@@ -96,6 +96,8 @@ export const getWishlistItems = async (req, res) => {
   const userId = req.user._id; // Assuming user ID is available from authentication middleware
 
   try {
+
+
 const user = await User.findById(userId)
   .populate({
     path: 'wishlistItems.product',
