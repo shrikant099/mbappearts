ac1181f7c0e5885b53b3413f12970d947b97e95d
krishna-65 <sharmakrishnakant721@gmail.com>
2025-09-08 12:56:45 +0530

    make changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,7 +45,7 @@ export const register = async (req, res)=>{
 
 
 
-        return res.json({success: true, user: {email: user?.profile?.email, name: user.name, token: token, accountType: user.accountType,_id:user._id}})
+        return res.json({success: true, user: {email: user?.profile?.email, name: user.name,  accountType: user.accountType,_id:user._id}})
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
